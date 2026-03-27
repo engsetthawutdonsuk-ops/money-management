26b66cead05b068dac3dd0039fa18bca4ec52dce
SETTHAWUT DONSUK <N061438@nsk.com>
2026-03-27 13:13:53 +0700

    fix: update firebase api key from console config

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAYF1wZnPuy8CINM8A3JLzRXfOFPzoaI9o",
+    apiKey: "AIzaSyAYF1wZnPuy8C1NM8A3JLzRXfOFPzoal9o",
     authDomain: "my-money-app-9954d.firebaseapp.com",
     databaseURL: "https://my-money-app-9954d-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "my-money-app-9954d",
-    storageBucket: "my-money-app-9954d.appspot.com",
+    storageBucket: "my-money-app-9954d.firebasestorage.app",
     messagingSenderId: "1036616583831",
     appId: "1:1036616583831:web:7f397748df5ac60e7837d2",
     measurementId: "G-SSGN58KXE5"
