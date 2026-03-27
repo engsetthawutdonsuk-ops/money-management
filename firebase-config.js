ccb66c2188782523c72eb4336d96619465d141e1
SETTHAWUT DONSUK <N061438@nsk.com>
2026-03-27 12:13:58 +0700

    fix: harden Google login config and auth flow

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAYF1wZnPuy8CINM8A3JLzRXfOFPzoaI9o",
+    authDomain: "my-money-app-9954d.firebaseapp.com",
+    databaseURL: "https://my-money-app-9954d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "my-money-app-9954d",
+    storageBucket: "my-money-app-9954d.appspot.com",
+    messagingSenderId: "1036616583831",
+    appId: "1:1036616583831:web:7f397748df5ac60e7837d2",
+    measurementId: "G-SSGN58KXE5"
+};
